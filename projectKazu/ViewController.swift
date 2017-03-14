4e542ad59af4324c420b7636c862a1b77ff05f58
kzhrsasaki <kzhr.sasaki@gmail.com>
2017-03-15 02:35:39 +0900

    3/14自習終了分

--- projectKazu/ViewController.swift
+++ projectKazu/ViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
     @IBOutlet weak var mySwitch: UISwitch!
     
     @IBOutlet weak var enterButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        myImageView.image = UIImage(named: "todoList.png")
 
     }
 
@@ -26,13 +29,20 @@ class ViewController: UIViewController {
             let notification:UILocalNotification = UILocalNotification()
             
             //タイトル
-            notification.alertTitle = "Fire"
+            notification.alertTitle = "TODO登録のご案内"
             //通知メッセージ
-            notification.alertBody = "ファイヤー！！"
+            notification.alertBody = "TODOが完了したら「履歴・完了登録画面」にて完了入力しましょう！"
             //Timezoneの設定
             notification.timeZone = TimeZone.current
+            
+            //フォーマットを設定
+            let df = DateFormatter()
+            df.dateFormat = "yy/MM/dd HH:mm:ss"
+
+            
             //毎日17時00分に通知を設定
-            notification.fireDate = Date(timeIntervalSinceNow: 10)
+            //notification.fireDate = Date(timeInterval: 61200, since: date(from: df.string(from: NSDate() as Date) + " 00:00:00")!)
+            
             //Notificationを表示（登録）
             UIApplication.shared.scheduleLocalNotification(notification)
             
